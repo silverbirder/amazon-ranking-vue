72de18f666fb40216fa507e82511bc885cc490d9
shibamoto <silverbirder@gmail.com>
2019-03-31 01:48:39 +0900

    add request

--- src/components/AmazonRanking.vue
+++ src/components/AmazonRanking.vue
@@ -1,29 +1,58 @@
 <template>
-  <div class="hello">
-  </div>
+    <div>
+        <a target="_blank" :href="data.url">
+            <div class="md-headline">{{data.category}}</div>
+        </a>
+        <md-card v-for="item in data.items">
+            <img :src="item.crown">
+            <md-card-media md-ratio="16:9">
+                <a target="_blank" :href="item.link">
+                    <img :src="item.src" :alt="item.title">
+                </a>
+            </md-card-media>
+            <md-card-header>
+                <div class="md-title">{{item.title}}</div>
+            </md-card-header>
+        </md-card>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'AmazonRanking',
-  data () {
-      // console.log(process.env.VUE_APP_AMAZON_RANKING_API)
-      // this.axios.get(process.env.VUE_APP_AMAZON_RANKING_API).then(response => {
-      //     console.log(response)
-      // })
-      const response = {
-          data: [],
-          message: '',
-          url: '',
-      }
-      return {
-          'APP_AMAZON_RANKING_API': process.env.VUE_APP_AMAZON_RANKING_API,
-      }
-  },
-  methods: {
-      // onUpdate: function() {
-      //     this.axios.get(this.APP_AMAZON_RANKING_API)
-      // }
-  }
-}
+    export default {
+        name: 'AmazonRanking',
+        data() {
+            return {
+                APP_AMAZON_RANKING_API: process.env.VUE_APP_AMAZON_RANKING_API,
+                data: {
+                    url: '',
+                    category: '',
+                    items: []
+                }
+            }
+        },
+        created() {
+            console.log(this.APP_AMAZON_RANKING_API)
+            this.axios
+                .get(this.APP_AMAZON_RANKING_API)
+                .then(response => {
+                    this.data = response.data.data
+                })
+                .catch(response => {
+                    alert("Not Fetch")
+                })
+        }
+    }
 </script>
+
+<style>
+    .card-expansion {
+        height: 320px;
+    }
+
+    .md-card {
+        width: 280px;
+        margin: 4px;
+        display: inline-block;
+        vertical-align: top;
+    }
+</style>
